5dbbea28e667dada6ad81b80c94fdaf8d5bd2e0a
Thanak Mech <thanakmech@gmail.com>
2024-01-20 23:55:46 +0700

    Fix bug and change door status + view

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useEffect, useState } from "react";
-import images from "/door.png";
+import door from "/door.png";
+import door_sold from "/sold door.png";
 import "./style.css";
 
 function App() {
   const array = [];
 
   const room = "Room ";
-  const roomLoop = 100;
+  const roomLoop = 500;
   for (let i = 1; i <= roomLoop; i++) {
     // console.log(i)
     array.push(i);
   }
 
-  const arraymap = array.map((arr, index) => ({
+  const arrayroom = array.map((arr, index) => ({
     ...arr,
     roomName: room + index,
-    id: index + 1,
+    id: crypto.randomUUID(),
     rented: false,
   }));
-  const [roomArray, setRoomArray] = useState(arraymap);
+
+  const [roomArray, setRoomArray] = useState(arrayroom);
   const [countRented, setCountRented] = useState(0);
   const [countAvailable, setCountAvailable] = useState(roomArray.length);
+  const [searchRoom, setSearchRoom] = useState("")
+
 
   function rentRoom(roomId) {
     const updatedRoom = roomArray.map((room) =>
       roomId === room.id
         ? {
             ...room,
-            rented: !room.rented,
+            rented: true,
           }
         : room
     );
@@ -41,29 +45,66 @@ function App() {
       roomId === room.id
         ? {
             ...room,
-            rented: !room.rented,
+            rented: false,
           }
         : room
     );
     setRoomArray(updatedRoom);
+    if(countRented <= 0 ) return;
     setCountRented((count) => count - 1);
     setCountAvailable((count) => count + 1);
   }
-
+  function handleSubmit(e){
+    e.preventDefault();
+    const filterRoom = roomArray.filter((room)=>{
+      searchRoom? room.id === parseInt(searchRoom) : ""
+    })
+    setRoomArray(filterRoom)
+    return filterRoom
+  }
   return (
     <>
       <div className="title">
+        <h1>Thanak Mech's Rental Management</h1>
         <div>Avaible Room: {countAvailable}</div>
         <div>Rented Room: {countRented}</div>
       </div>
-
+      <div className="searchRoom">
+        <form onSubmit={handleSubmit}>
+          <input type="text"
+          pattern="[0-9]*" 
+          placeholder="Search Room By ID"
+          value={searchRoom}
+          onChange={(e)=>setSearchRoom(e.target.value)}
+          />
+          </form>
+        </div>
+        <div className="view">
+          <span>View: </span>
+          <button>All</button>
+          <button>All Available Room</button>
+          <button>All Rented Room</button>
+        </div>
       <div className="container">
-        {roomArray.map((room) => (
+        {roomArray.map((room,index) => (
           <div key={room.id} className="room-card">
-            <img src={images} alt="Door" height={80} />
-            <div>ID: {room.id}</div>
-            <div>{room.roomName}</div>
-            {room.rented ? <div className="unavailable">Unavailable</div> : <div className="available">Available</div>}
+            {/* <div className="room_owner_title" >
+              <p>Room Owner</p>
+              <p>Ronaldo</p>
+            </div> */}
+            {room.rented ? (
+              <img src={door_sold}  className="door" alt="Door" height={140} />
+            ):(
+              <img src={door} className="door" alt="Door_Sold" height={140} />
+            )}
+            <div>ID: {index+1}</div>
+            <div>Room: {index+1}</div>
+            {/* <div>{room.roomName}</div> */}
+            {room.rented === true? (
+              <div className="unavailable">Unavailable</div>
+            ) : (
+              <div className="available">Available</div>
+            )}
             {!room.rented ? (
               <button onClick={() => rentRoom(room.id)}>Rent Room</button>
             ) : (
@@ -71,6 +112,7 @@ function App() {
             )}
           </div>
         ))}
+       
       </div>
     </>
   );
